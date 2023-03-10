a16392f8703f3fed0713c15436f03a85bc9666e1
onion <62121953+oniongithub@users.noreply.github.com>
2023-03-10 18:33:00 -0500

    Update sh_config.lua

--- lua/sh_config.lua
+++ lua/sh_config.lua
@@ -1,7 +1,7 @@
 -- Rank Settings
 ors.rank_int = { "user", "vip", "backer" }
 ors.rank_str = { ["user"] = 1, ["vip"] = 2, ["backer"] = 3 }
-ors.base_rank = "user" -- user index
+ors.base_rank = "user"
 
 -- DB Settings
 ors.table_name = "onion_ranks"
